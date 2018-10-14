acb7ed9f64279fa3a5cacf7a93f89531eb336d1a
Narate Ketram <koonnarate@gmail.com>
2018-10-14 14:30:03 +0700

    Add reload-prom.sh - Fixed IP for run-prom.sh

--- run-prom.sh
+++ run-prom.sh
@@ -6,9 +6,9 @@ mkdir -p prometheus_data
 chown -R 65534:65534 prometheus_data
 
 docker run -d --restart=always \
-    --name prom \
-    -v $(pwd)/prometheus.yml:/prometheus.yml \
+    --net=host \
+    --name=prom \
+    -v $(pwd)/conf:/conf \
     -v $(pwd)/prometheus_data:/prometheus \
-    -p 9090:9090 \
-    prom/prometheus --config.file=/prometheus.yml --storage.tsdb.retention=30d --web.listen-address="$IP:9090"
+    prom/prometheus --config.file=/conf/prometheus.yml --storage.tsdb.retention=30d --web.listen-address="$IP:9090" --web.enable-lifecycle
 
